81447d80157cc1b455e6c1cc1ec683f09cf59a70
max <max13021302@gmail.com>
2026-03-07 21:44:29 +0200

    chore: ignore and untrack generated docs build artifacts

--- apps/docs/build/assets/js/aba21aa0.9ad72362.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrug_radar_docs=self.webpackChunkrug_radar_docs||[]).push([[742],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
